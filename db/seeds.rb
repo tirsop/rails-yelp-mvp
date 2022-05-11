86aae4a55eb10117eb42c17101651db6b9ebce83
Tirso Pascual <tpascualgarcia@gmail.com>
2022-05-11 16:33:48 +0900

    first version with 2 models working. fucked up css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning the DB..."
+Restaurant.destroy_all
+
+puts "Creating pets..."
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::CATEGORY.sample
+  )
+end
+puts "...created #{Restaurant.count} pets."
